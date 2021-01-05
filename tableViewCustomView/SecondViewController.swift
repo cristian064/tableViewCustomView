3a1f3b2d2548613f74992fc2fb9676dcd524444e
Cristian Ayala <cayala@canvia.com>
2021-01-04 20:08:59 -0500

    se agrego nuevos elementos

---
+++ tableViewCustomView/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  tableViewCustomView
+//
+//  Created by Cristian Ayala Laura on 1/4/21.
+//  Copyright © 2021 cristian ayala. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
